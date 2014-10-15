573e94d17d523071ee4a7294f06efb8b7e317b2e
Dave Bassant <davebassant@gmail.com>
2014-10-15 22:25:53 +0100

    added sign-in ability, code needs inspecting before moving on to authd posing

--- app/src/main/java/com/poseit/poseit_dev/AppConstants.java
+++ app/src/main/java/com/poseit/poseit_dev/AppConstants.java
@@ -1,5 +1,13 @@
 package com.poseit.poseit_dev;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.http.HttpTransport;
@@ -18,11 +26,47 @@ public class AppConstants {
      */
     public static final HttpTransport HTTP_TRANSPORT = AndroidHttp.newCompatibleTransport();
 
+    public static final String WEB_CLIENT_ID = "645929148555-oum3ijnvf6l2votnfpq60m71tp10l0d1.apps.googleusercontent.com";
+
+    public static final String AUDIENCE = "server:client_id:" + WEB_CLIENT_ID;
+
     public static Yorn getApiServiceHandle() {
         // Use a builder to help formulate the API request.
         Yorn.Builder yornBuilder = new Yorn.Builder(AppConstants.HTTP_TRANSPORT,
                 AppConstants.JSON_FACTORY, null);
 
         return yornBuilder.build();
+    }
+
+    public static int countGoogleAccounts(Context context) {
+        AccountManager am = AccountManager.get(context);
+        Account[] accounts = am.getAccountsByType(GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE);
+        if (accounts == null || accounts.length < 1) {
+            return 0;
+        } else {
+            return accounts.length;
+        }
+    }
+
+    public static boolean checkGooglePlayServicesAvailable(Activity activity) {
+        final int connectionStatusCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(activity);
+        if (GooglePlayServicesUtil.isUserRecoverableError(connectionStatusCode)) {
+            showGooglePlayServicesAvailabilityErrorDialog(activity, connectionStatusCode);
+            return false;
+        }
+        return true;
+    }
+
+    public static void showGooglePlayServicesAvailabilityErrorDialog(final Activity activity,
+                                                                     final int connectionStatusCode) {
+        final int REQUEST_GOOGLE_PLAY_SERVICES = 0;
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Dialog dialog = GooglePlayServicesUtil.getErrorDialog(
+                        connectionStatusCode, activity, REQUEST_GOOGLE_PLAY_SERVICES);
+                dialog.show();
+            }
+        });
     }
 }
--- app/src/main/java/com/poseit/poseit_dev/PoseActivity.java
+++ app/src/main/java/com/poseit/poseit_dev/PoseActivity.java
@@ -1,6 +1,9 @@
 package com.poseit.poseit_dev;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,16 +11,28 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.appspot.numeric_ion_678.yorn.Yorn;
 import com.appspot.numeric_ion_678.yorn.Yorn.YornEndpoint.NewQuestionSimple;
 import com.appspot.numeric_ion_678.yorn.model.Message;
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.common.AccountPicker;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.common.base.Strings;
 
 import java.io.IOException;
 
 public class PoseActivity extends Activity {
 
     private static final String LOG_TAG = "PoseActivity";
+
+    private static final int ACTIVITY_RESULT_FROM_ACCOUNT_SELECTION = 2222;
+
+    private AuthorizationCheckTask mAuthTask;
+    private String mEmailAccount = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,5 +87,167 @@ public class PoseActivity extends Activity {
                 };
 
         setNewQuestion.execute();
+    }
+
+    public void performAuthCheck(String emailAccount) {
+        // Cancel previously running tasks.
+        if (mAuthTask != null) {
+            try {
+                mAuthTask.cancel(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        new AuthorizationCheckTask().execute(emailAccount);
+    }
+
+    public void onClickSignIn(View view) {
+        TextView emailAddressTV = (TextView) view.getRootView().findViewById(R.id.email_address_tv);
+        // Check to see how many Google accounts are registered with the device.
+        int googleAccounts = AppConstants.countGoogleAccounts(this);
+        if (googleAccounts == 0) {
+            // No accounts registered, nothing to do.
+            Toast.makeText(this, R.string.toast_no_google_accounts_registered,
+                    Toast.LENGTH_LONG).show();
+        } else if (googleAccounts == 1) {
+            // If only one account then select it.
+            Toast.makeText(this, R.string.toast_only_one_google_account_registered,
+                    Toast.LENGTH_LONG).show();
+            AccountManager am = AccountManager.get(this);
+            Account[] accounts = am.getAccountsByType(GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE);
+            if (accounts != null && accounts.length > 0) {
+                // Select account and perform authorization check.
+                emailAddressTV.setText(accounts[0].name);
+                mEmailAccount = accounts[0].name;
+                performAuthCheck(accounts[0].name);
+            }
+        } else {
+            // More than one Google Account is present, a chooser is necessary.
+
+            // Reset selected account.
+            emailAddressTV.setText(R.string.auth_pending);
+
+            // Invoke an {@code Intent} to allow the user to select a Google account.
+            Intent accountSelector = AccountPicker.newChooseAccountIntent(null, null,
+                    new String[]{GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE}, false,
+                    "Select an account to sign in with", null, null, null);
+            startActivityForResult(accountSelector,
+                    ACTIVITY_RESULT_FROM_ACCOUNT_SELECTION);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ACTIVITY_RESULT_FROM_ACCOUNT_SELECTION && resultCode == RESULT_OK) {
+            // This path indicates the account selection activity resulted in the user selecting a
+            // Google account and clicking OK.
+
+            // Set the selected account.
+            String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+            TextView emailAccountTextView = (TextView)this.findViewById(R.id.email_address_tv);
+            emailAccountTextView.setText(accountName);
+
+            // Fire off the authorization check for this account and OAuth2 scopes.
+            performAuthCheck(accountName);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mAuthTask!=null) {
+            mAuthTask.cancel(true);
+            mAuthTask = null;
+        }
+    }
+
+    private boolean isSignedIn() {
+        if (!Strings.isNullOrEmpty(mEmailAccount)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    class AuthorizationCheckTask extends AsyncTask<String, Integer, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... emailAccounts) {
+            Log.i(LOG_TAG, "Background task started.");
+
+            if (!AppConstants.checkGooglePlayServicesAvailable(PoseActivity.this)) {
+                return false;
+            }
+
+            String emailAccount = emailAccounts[0];
+            // Ensure only one task is running at a time.
+            mAuthTask = this;
+
+            // Ensure an email was selected.
+            if (Strings.isNullOrEmpty(emailAccount)) {
+                publishProgress(R.string.toast_no_google_account_selected);
+                // Failure.
+                return false;
+            }
+
+            Log.d(LOG_TAG, "Attempting to get AuthToken");
+
+            try {
+                // If the application has the appropriate access then a token will be retrieved, otherwise
+                // an error will be thrown.
+                GoogleAccountCredential credential = GoogleAccountCredential.usingAudience(
+                        PoseActivity.this, AppConstants.AUDIENCE);
+                credential.setSelectedAccountName(emailAccount);
+
+                String accessToken = credential.getToken();
+
+                Log.d(LOG_TAG, "AccessToken retrieved");
+
+                // Success.
+                return true;
+            } catch (GoogleAuthException unrecoverableException) {
+                Log.e(LOG_TAG, "Exception checking OAuth2 authentication.", unrecoverableException);
+                publishProgress(R.string.toast_exception_checking_authorization);
+                // Failure.
+                return false;
+            } catch (IOException ioException) {
+                Log.e(LOG_TAG, "Exception checking OAuth2 authentication.", ioException);
+                publishProgress(R.string.toast_exception_checking_authorization);
+                // Failure or cancel request.
+                return false;
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... stringIds) {
+            // Toast only the most recent.
+            Integer stringId = stringIds[0];
+            Toast.makeText(PoseActivity.this, stringId, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            mAuthTask = this;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            TextView emailAddressTV = (TextView) PoseActivity.this.findViewById(R.id.email_address_tv);
+            if (success) {
+                // Authorization check successful, set internal variable.
+                mEmailAccount = emailAddressTV.getText().toString();
+            } else {
+                // Authorization check unsuccessful, reset TextView to empty.
+                emailAddressTV.setText("");
+            }
+            mAuthTask = null;
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+        }
     }
 }
